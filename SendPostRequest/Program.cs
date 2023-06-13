69385064d4937257d2eb6c4925d05bad269b63be
Nika Gagua <nika.gagua@solutions2share.net>
2023-06-13 21:20:55 +0400

    Add project files.

---
+++ SendPostRequest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace SendPostRequest
+{
+    internal static class SendPostRequest
+    {
+        static void Main(string[] args)
+        {
+            var postData = new PostData
+            {
+                Name = "John Doe",
+                Age = 30,
+                Address = "123 Main St"
+            };
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+
+            var json = JsonSerializer.Serialize(postData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+            var response = client.PostAsync("posts", content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var responseContent = response.Content.ReadAsStringAsync().Result;
+                var postResponse = JsonSerializer.Deserialize<PostResponse>(responseContent, options);
+                Console.WriteLine("Id: " + postResponse.Id);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+        }
+    }
+}
